e0a90214ea4ce5dbf5eb89efa0eb33877455fd33
Meg MacIver <megmaciver13@gmail.com>
2018-01-12 08:55:14 -0500

    develop edit and post functionality (still working on directions and ingredients

--- src/Components/EditPost/EditPost.js
+++ src/Components/EditPost/EditPost.js
@@ -7,22 +7,86 @@ class EditPost extends Component {
   constructor (props) {
     super(props)
 
-    console.log(this.props)
+    this.state = {
+      post: {
+        title: '',
+        dateMonth: null,
+        dateNumber: null,
+        dateYear: null,
+        featuredImage: '',
+        introText: '',
+        additionalImage: '',
+        yieldQuantity: '',
+        ingredients: [],
+        finalImage: '',
+        finalText: '',
+        directions: []
+      }
+    }
+
     this.handleDelete = this.handleDelete.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleStateUpdate = this.handleStateUpdate.bind(this)
+    this.handleStateArrayUpdate = this.handleStateArrayUpdate.bind(this)
+    this.handleStateArrayOfObjectsUpdate = this.handleStateArrayOfObjectsUpdate.bind(this)
+  }
+
+  componentDidMount () {
+    axios.get(`http://localhost:3001/posts/${this.props.match.params.title}`)
+      .then(response => {
+        console.log(response.data)
+        var res = response.data
+        this.setState({
+          post: {
+            title: res.title,
+            dateMonth: res.date.substring(5, 7),
+            dateNumber: res.date.substring(8, 10),
+            dateYear: res.date.substring(0, 4),
+            featuredImage: res.featuredImage,
+            introText: res.introText,
+            additionalImage: res.additionalImage,
+            yieldQuantity: res.yieldQuantity,
+            ingredients: res.ingredients,
+            finalImage: res.finalImage,
+            finalText: res.finalText,
+            directions: res.directions
+          }
+        })
+      })
+      .catch(err => console.log(err))
   }
 
   handleStateUpdate (e) {
     this.setState({
-      [e.target.name]: e.target.value
+      post: {
+        ...this.state.post,
+        [e.target.name]: e.target.value
+      }
     })
+  }
+
+  handleStateArrayUpdate (e) {
+    let items = this.state.post[e.target.name]
+    items[e.target.index] = e.target.value
+
+    console.log(items)
+    this.forceUpdate()
+  }
+
+  handleStateArrayOfObjectsUpdate (e) {
+    let items = this.state.post[e.target.name]
+    items[e.target.index][e.target.type] = e.target.value
+
+    console.log(items)
+    this.forceUpdate()
   }
 
   handleSubmit (e) {
     e.preventDefault()
-    axios.put(`http://localhost:3001/posts/${this.state.title}`, this.state)
+    axios.put(`http://localhost:3001/posts/${this.props.match.params.title}`, this.state)
       .then(response => {
         console.log(response)
-        this.props.history.push(`/${this.state.title}`)
+        this.props.history.push(`/posts/${this.state.title}`)
       })
       .catch(err => console.log(err))
   }
@@ -30,7 +94,7 @@ class EditPost extends Component {
   handleDelete (e) {
     e.preventDefault()
 
-    axios.delete(`/posts/${this.props.match.params.title}`, {title: this.props.match.params.title})
+    axios.delete(`http://localhost:3001/posts/${this.props.match.params.title}`, {title: this.props.match.params.title})
       .then(() => {
         this.props.history.push('/')
       })
@@ -38,43 +102,63 @@ class EditPost extends Component {
   }
 
   render () {
+    let showIngredientInputs = this.state.post.ingredients.map((ingredient, key) => {
+      return (
+        <div key={key}>
+          <input name='ingredients' index={key} defaultValue={ingredient} onChange={e => this.handleStateArrayUpdate(e)} /><br />
+        </div>
+      )
+    })
+    let showDirectionInputs = this.state.post.directions.map((direction, key) => {
+      return (
+        <label>
+          Step {key + 1}:
+          <input name='directions' index={key} type='text' value={direction.text} onChange={e => this.handleStateArrayOfObjectsUpdate(e)} /><br />
+          <input name='directions' index={key} type='imageUrl' value={direction.imageUrl} onChange={e => this.handleStateArrayOfObjectsUpdate(e)} /><br />
+        </label>
+      )
+    })
     return (
       <div className='form-container'>
         <h1>Edit <span className='italics'>{this.props.match.params.title}</span></h1>
         <form onSubmit={e => this.handleSubmit(e)}>
           <label>
-            Post Title: <input name='title' placeholder='new title' onChange={e => this.handleStateUpdate(e)} />
+            Post Title: <input name='title' defaultValue={this.state.post.title} onChange={e => this.handleStateUpdate(e)} />
           </label>
           <br />
           <label>
-            Featured Image URL: <input name='featuredImage' placeholder='new featured image' onChange={e => this.handleStateUpdate(e)} />
+            Featured Image URL: <input name='featuredImage' value={this.state.post.featuredImage} onChange={e => this.handleStateUpdate(e)} />
           </label>
           <br />
           <label>
-            Intro Text: <input name='introText' placeholder='new intro text' onChange={e => this.handleStateUpdate(e)} />
+            Intro Text: <input name='introText' value={this.state.post.introText} onChange={e => this.handleStateUpdate(e)} />
           </label>
           <br />
           <label>
-            Additional Image URL (before ingredients): <input name='additionalImage' placeholder='new additional image' onChange={e => this.handleStateUpdate(e)} />
+            Additional Image URL: <input name='additionalImage' value={this.state.post.additionalImage} onChange={e => this.handleStateUpdate(e)} />
           </label>
           <br />
           <label>
-            Yield Quantity: <input name='yieldQuantity' placeholder='new yield quantity' onChange={e => this.handleStateUpdate(e)} />
-          </label>
-          <br />
-          {/* Ingredients: <input name='ingredients' onChange={e => this.handleStateUpdate} /><br /> */}
-          <br />
-          <label>
-            Final Image URL: <input name='finalImage' placeholder='new final image' onChange={e => this.handleStateUpdate(e)} />
+            Yield Quantity: <input name='yieldQuantity' value={this.state.post.yieldQuantity} onChange={e => this.handleStateUpdate(e)} />
           </label>
           <br />
           <label>
-            Final Text: <input name='finalText' placeholder='new final text' onChange={e => this.handleStateUpdate(e)} />
+            Ingredients: {showIngredientInputs}
+          </label>
+          <label>
+            Final Image URL: <input name='finalImage' value={this.state.post.finalImage} onChange={e => this.handleStateUpdate(e)} />
           </label>
           <br />
-          {/* Directions: <input name='directions' onChange={e => this.handleStateUpdate} /><br /> */}
+          <label>
+            Final Text: <input name='finalText' value={this.state.post.finalText} onChange={e => this.handleStateUpdate(e)} />
+          </label>
           <br />
-          <input type='submit' value='Create New Post' className='submit-button' />
+          <label>
+            Directions: <br />
+            {showDirectionInputs}
+          </label>
+          <br />
+          <input type='submit' value='Update Post' className='submit-button' />
         </form>
         <br />
         <input type='submit' value='Delete' className='submit-button' onClick={e => this.handleDelete(e)} />
--- src/Components/NewPost/NewPost.js
+++ src/Components/NewPost/NewPost.js
@@ -8,35 +8,62 @@ class NewPost extends Component {
     super(props)
 
     this.state = {
-      title: '',
-      featuredImage: '',
-      introText: '',
-      additionalImage: '',
-      yieldQuantity: '',
-      ingredients: [],
-      finalImage: '',
-      finalText: '',
-      directions: []
+      post: {
+        title: '',
+        featuredImage: '',
+        introText: '',
+        additionalImage: '',
+        yieldQuantity: '',
+        ingredients: [],
+        finalImage: '',
+        finalText: '',
+        directions: []
+      },
+
     }
 
     this.handleStateUpdate = this.handleStateUpdate.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
   }
 
+  addIngredientInput () {
+
+  }
+
+  addDirectionInput () {
+
+  }
+
   handleStateUpdate (e) {
     this.setState({
-      [e.target.name]: e.target.value
+      post: {
+        ...this.state.post,
+        [e.target.name]: e.target.value
+      }
+    })
+  }
+
+  handleArrayStateUpdate (e) {
+    this.setState({
+      post: {
+        ...this.state.post,
+        [e.target.name]: [...this.state.post[e.target.name], e.target.value]
+      }
     })
   }
 
   handleSubmit (e) {
     e.preventDefault()
-    axios.post(`http://localhost:3001/posts/${this.state.title}`, this.state)
-      .then(response => {
-        console.log(response)
-        this.props.history.push(`/${this.state.title}`)
-      })
-      .catch(err => console.log(err))
+    if (this.state.post.title.length >= 1) {
+      axios.post(`http://localhost:3001/posts`, this.state.post)
+        .then(response => {
+          console.log(response)
+          this.props.history.push(`/posts/${this.state.post.title}`)
+        })
+        .catch(err => console.log(err))
+    } else {
+      window.alert('Post must have a title!')
+    }
   }
 
   render () {
@@ -46,36 +73,42 @@ class NewPost extends Component {
         <h1>New Post</h1>
         <form onSubmit={e => this.handleSubmit(e)}>
           <label>
-            Post Title: <input name='title' placeholder='new title' onChange={e => this.handleStateUpdate(e)} />
+            Post Title: <input name='title' placeholder='Title' onChange={e => this.handleStateUpdate(e)} />
           </label>
           <br />
           <label>
-            Featured Image URL: <input name='featuredImage' placeholder='new featured image' onChange={e => this.handleStateUpdate(e)} />
+            Featured Image URL: <input name='featuredImage' placeholder='URL' onChange={e => this.handleStateUpdate(e)} />
           </label>
           <br />
           <label>
-            Intro Text: <input name='introText' placeholder='new intro text' onChange={e => this.handleStateUpdate(e)} />
+            Intro Text: <input name='introText' placeholder='Intro Text' onChange={e => this.handleStateUpdate(e)} />
           </label>
           <br />
           <label>
-            Additional Image URL (before ingredients): <input name='additionalImage' placeholder='new additional image' onChange={e => this.handleStateUpdate(e)} />
+            Post-Intro Image URL: <input name='additionalImage' placeholder='URL' onChange={e => this.handleStateUpdate(e)} />
           </label>
           <br />
           <label>
-            Yield Quantity: <input name='yieldQuantity' placeholder='new yield quantity' onChange={e => this.handleStateUpdate(e)} />
+            Yield Quantity: <input name='yieldQuantity' placeholder='Recipe Yield' onChange={e => this.handleStateUpdate(e)} />
           </label>
           <br />
-          {/* Ingredients: <input name='ingredients' onChange={e => this.handleStateUpdate} /><br /> */}
+          {/* <label>
+            Ingredients: <input name='post[ingredients]' placeholder='Ingredient' onChange={e => this.handleArrayStateUpdate(e)} />
+            <button onChange={() => this.addIngredientInput()}>Add New Ingredient</button>
+          </label> */}
           <br />
           <label>
-            Final Image URL: <input name='finalImage' placeholder='new final image' onChange={e => this.handleStateUpdate(e)} />
+            Final Image URL: <input name='finalImage' placeholder='URL' onChange={e => this.handleStateUpdate(e)} />
           </label>
           <br />
           <label>
-            Final Text: <input name='finalText' placeholder='new final text' onChange={e => this.handleStateUpdate(e)} />
+            Final Text: <input name='finalText' placeholder='Final Text' onChange={e => this.handleStateUpdate(e)} />
           </label>
           <br />
-          {/* Directions: <input name='directions' onChange={e => this.handleStateUpdate} /><br /> */}
+          {/* <label>
+            Directions: <input name='post[directions]' placeholder='One Step At a Time' onChange={e => this.handleArrayStateUpdate(e)} />
+            <button onChange={() => this.addDirectionInput()}>Add New Direction</button>
+          </label> */}
           <br />
           <input type='submit' value='Create New Post' className='submit-button' />
         </form>
